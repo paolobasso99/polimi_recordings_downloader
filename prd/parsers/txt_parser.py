1ac85251e4baa539c36d9e4a6b1398727afc2bf7
Paolo Basso <bassopaolo99@gmail.com>
2022-09-08 22:43:09 +0200

    Multithreading for txt files

--- prd/parsers/txt_parser.py
+++ prd/parsers/txt_parser.py
@@ -1,15 +1,36 @@
+from itertools import repeat
 from typing import List
 from Recording import Recording
 from webex_api import extract_id_from_url, generate_recording_from_id
 import typer
 import requests
+from multiprocessing.pool import ThreadPool
+
+def get_video_ids_from_file(file:str) -> List[str]:
+    """Get the video ids from a txt file.
+
+    Args:
+        file (str): The file path.
+
+    Returns:
+        List[str]: A list of video ids.
+    """
+    video_ids: List[str] = []
+    with open(file) as f:
+        for i, line in enumerate(f):
+            line = line.rstrip()
+            if line.startswith("http"):
+                video_ids.append(extract_id_from_url(line))
+            elif len(line) == 32:
+                video_ids.append(line)
+            elif len(line) != 32 and len(line) > 0:
+                typer.echo(f'Invalid line found, line number {i+1} is "{line}".')
+        
+    return video_ids
+    
 
 
-def recordings_from_txt(
-    file: str,
-    course: str,
-    academic_year: str
-) -> List[Recording]:
+def recordings_from_txt(file: str, course: str, academic_year: str) -> List[Recording]:
     """Get the recordings from the TXT file.
 
     Args:
@@ -20,30 +41,18 @@ def recordings_from_txt(
     Returns:
         List[Recording]: Recording objects extracted from the file.
     """
-    recordings: List[Recording] = []
-    with open(file) as f:
-        video_ids: List[str] = []
-        for i, line in enumerate(f):
-            line = line.rstrip()
-            if line.startswith("http"):
-                video_ids.append(extract_id_from_url(line))
-            elif len(line) == 32:
-                video_ids.append(line)
-            elif len(line) != 32 and len(line) > 0:
-                typer.echo(f"Invalid line found, line number {i+1} is \"{line}\".")
-        typer.echo(f"Found {len(video_ids)} urls in the input file.")
-        typer.echo("Generating recording download links, this may take a bit...")
+    video_ids: List[str] = get_video_ids_from_file(file)
+    typer.echo(f"Found {len(video_ids)} urls in the input file.")
+    typer.echo("Generating recording download links, this may take a bit...")
 
-        for id in video_ids:
-            try:
-                recording: Recording = generate_recording_from_id(
-                    video_id=id,
-                    academic_year=academic_year,
-                    course=course
-                )
-                recordings.append(recording)
-            except requests.exceptions.ConnectionError as e:
-                typer.echo(str(e))
-                raise typer.Exit(code=1)
+    try:
+        pool: ThreadPool = ThreadPool()
+        recordings: List[Recording] = pool.starmap(
+            generate_recording_from_id,
+            zip(video_ids, repeat(academic_year), repeat(course)),
+        )
+    except requests.exceptions.ConnectionError as e:
+        typer.echo(str(e))
+        raise typer.Exit(code=1)
 
     return recordings
